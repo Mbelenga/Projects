9ea524dfcc0c2890949e7caa62b71e262a059906
Mbelenga <mbelengaterence@gmail.com>
2024-02-22 15:21:59 +0300

    Variables in dart

---
+++ DART/main.dart
@@ -0,0 +1,5 @@
+// My first dart code
+
+void main() {
+  print("Hello World");
+}
---
+++ DART/variables.dart
@@ -0,0 +1,16 @@
+void main(){
+  // Variables in the dart programming language
+  String school = "PLP AFRICA ACADEMY";
+  int total = 400;
+  double GPA = 4.0;
+  num year = 2024;
+  bool isMarried = 'true';
+  var a = "Thursday";
+  var b = "Wednesday"
+  // Print our variables
+  print(school);
+  print(total);
+  print(GPA);
+  print(year);
+  print(isMarried);
+}
