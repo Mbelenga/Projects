d20a4147ca18edef35508f89448bcd21b2670a9d
Mbelenga <mbelengaterence@gmail.com>
2025-06-13 18:34:49 +0300

    Progress

---
+++ simple-auth-system/app.py
@@ -0,0 +1,11 @@
+username = input('Enter username: ')
+password = input('Enter Password: ')
+
+print('Your account has been created successfully')
+print('Login Now!')
+
+username2 = input('Enter username: ')
+password2 = input('Enter Password: ')
+
+if username == username2 and password == password2:
+    print('Logged in successfully')
